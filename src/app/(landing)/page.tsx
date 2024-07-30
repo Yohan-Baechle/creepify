c979c22ce357e30df0597d412d2c84e5f78c1aba
Yohan-Baechle <baechle.yohan@gmail.com>
2024-07-30 11:22:08 +0200

    Hero ok

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/landing/Hero"
+
 export default function Home() {
-    return <main className="container"></main>
+    return (
+        <main className="container">
+            <Hero />
+        </main>
+    )
 }
